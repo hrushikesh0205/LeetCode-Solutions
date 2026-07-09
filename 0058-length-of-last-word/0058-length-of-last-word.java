d6fa1e882eccd1ac9a7f84fb083c166890336afc
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-07-09 20:33:48 +0530

    Time: 0 ms (100%), Space: 43.3 MB (17.24%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        int count=0;
+        int end=s.length()-1;
+        
+        while(end>=0 && s.charAt(end)==' ')
+        {
+            end--;
+        }
+        while(end>=0 && s.charAt(end)!=' ')
+        {
+            count++;
+            end--;
+        }
+        return count;
+    }
+}
