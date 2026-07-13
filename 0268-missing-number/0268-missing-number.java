489cec88faacd3eb1f37ff5e843865dc2d221b51
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-07-14 02:12:21 +0530

    Time: 0 ms (100%), Space: 47.2 MB (82.14%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
 
-        int count=0;
-
-        for(int i=0; i<nums.length; i++)
-        {
-            count=count+nums[i];
-        }
-
-        int actualvalue=(nums.length*(nums.length+1))/2;
-        int missing=actualvalue-count;
-        return missing;
-
+      int count=0;
+      for(int i=0; i<nums.length; i++)
+      {
+          count=count+nums[i];
+      }
+      int actual=(nums.length*(nums.length+1))/2;
+      int result=actual-count;
+      return result;
     }
 }
