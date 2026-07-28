6306f64fa8623a8a5c2bd6bd5ffa0ab3b68081c7
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-07-29 00:07:28 +0530

    Time: 2 ms (92.11%), Space: 48 MB (20.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,7 +2,6 @@ class Solution {
     public void moveZeroes(int[] nums) {
   
   int left=0;
-
   for(int right=0; right<nums.length; right++)
   {
     if(nums[right]!=0)
@@ -10,10 +9,9 @@ class Solution {
         int temp=nums[left];
         nums[left]=nums[right];
         nums[right]=temp;
-    
+
         left++;
     }
   }
-  
 }
 }
