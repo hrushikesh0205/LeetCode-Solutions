163a0b2aba9fb81f1f46db402467805793ebd921
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-08-07 22:35:27 +0530

    Time: 3 ms (86.04%), Space: 43.3 MB (41.59%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,30 @@
-
 class Solution {
     public boolean isValid(String s) {
-
-        Stack<Character> ab = new Stack<>();
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
-                ab.push(arr[i]);
-            }
-            else {
-                if (ab.isEmpty()) {
-                    return false;
-                }
-                char top = ab.pop();
-                if ((arr[i] == ')' && top != '(') ||
-                (arr[i] == ']' && top != '[') ||
-                (arr[i] == '}' && top != '{')) 
-                {
-                    return false;
-                }
-            }
+    
+    Stack<Character>ab=new Stack<>();
+    char[]nums=s.toCharArray();
+    for(int i=0; i<nums.length; i++)
+    {
+        if(nums[i]=='(' || nums[i]=='{'|| nums[i]=='[')
+        {
+            ab.push(nums[i]);
         }
-        return ab.isEmpty();
+        else
+        {
+            if(ab.isEmpty())
+            {
+                return false;
+            }
+
+        char top=ab.pop();
+             if((nums[i]==')' && top!='(') ||
+               (nums[i]=='}' && top!='{') ||
+               (nums[i]==']' && top!='['))
+               {
+                return false;
+               }
+        }
+    }
+    return ab.isEmpty();
     }
 }
