d3696a50e557bd199292d61c917f23b957bffef7
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-09-12 12:55:51 +0530

    Time: 0 ms (100%), Space: 47.7 MB (53.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -4,25 +4,25 @@ class Solution {
         int left=0;
         int right=height.length-1;
 
+        int leftmax=height[left];
+        int rightmax=height[right];
         int water=0;
-        int leftmax=height[left], rightmax=height[right];
 
         while(left<right)
         {
             if(leftmax<rightmax)
             {
-                left++;
-                leftmax=Math.max(leftmax,height[left]);
-                water+=leftmax-height[left];
+               left++;
+               leftmax=Math.max(leftmax,height[left]);
+               water+=leftmax-height[left];
             }
             else
             {
                 right--;
                 rightmax=Math.max(rightmax,height[right]);
                 water+=rightmax-height[right];
-            }
+            }          
         }
-
         return water;
     }
 }
